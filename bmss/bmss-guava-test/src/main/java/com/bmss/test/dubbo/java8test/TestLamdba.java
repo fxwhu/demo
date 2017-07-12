c0200a32a9e2f91bb66a11b6109cc8e8780e3a60
fengxuan03 <fengxuan03@meituan.com>
2017-07-12 09:58:08 +0800

    添加测试b

--- bmss/bmss-guava-test/src/main/java/com/bmss/test/dubbo/java8test/TestLamdba.java
+++ bmss/bmss-guava-test/src/main/java/com/bmss/test/dubbo/java8test/TestLamdba.java
@@ -1,11 +1,17 @@
 package com.bmss.test.dubbo.java8test;
 
+
+import java.util.Arrays;
+
 /**
  * Created by fengxuan1 on 2016/11/16.
  */
 public class TestLamdba {
     public static void main(String[] args) {
-//        Arrays.asList("a", "b", "c").forEach(e -> System.out.println(e) );
+        Arrays.asList("a", "b", "c").forEach(e -> System.out.println(e) );
         System.out.println("=========");
+
+
+        System.out.println();
     }
 }
